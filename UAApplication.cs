29a9d581953632a846b876123fc36fc4ff6d7b21
Goetz Goerisch <g.goerisch@vdw.de>
2024-07-01 10:30:08 +0000

    refactor: linting C#

--- UAApplication.cs
+++ UAApplication.cs
@@ -83,7 +83,8 @@ namespace Opc.Ua.Cloud.Publisher
             }
 
             // create UA app
-            UAApplicationInstance = new ApplicationInstance {
+            UAApplicationInstance = new ApplicationInstance
+            {
                 ApplicationName = Settings.Instance.PublisherName,
                 ApplicationType = ApplicationType.Client,
                 ConfigSectionName = "UACloudPublisher"
